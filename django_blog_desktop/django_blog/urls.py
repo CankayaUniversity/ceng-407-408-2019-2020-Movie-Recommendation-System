4a3c769979b06af71b5edd7a724ff40b6d1f58a7
dorukgursey <dorukgursey@gmail.com>
2020-04-24 15:36:12 +0300

    1st Release
    Movie Recommendation System 1st Testable Release
    
    Co-Authored-By: Kaan Önder <kaan0nder@users.noreply.github.com>
    Co-Authored-By: berkesaka <berkesaka@users.noreply.github.com>
    Co-Authored-By: Önen Emre Can <onenemre@users.noreply.github.com>

---
+++ django_blog_desktop/django_blog/urls.py
@@ -0,0 +1,40 @@
+"""django_blog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from blog import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('blog/',views.post_list , name='post_list') ,
+    path('recommend/',views.recommend , name='recommend'),
+    path('movie_page',views.movie_page , name='movie_page'),
+    path('register/', views.register, name='register'),
+    path('movie_liked/', views.movie_liked, name='movie_liked'),
+    path('buttonRefresh/', views.buttonRefresh, name='buttonRefresh'),
+    path('getrecommendation/', views.getrecommendations, name='getrecommendations'),
+    path('',include("django.contrib.auth.urls")),
+    path('friendsimilarity/', views.friend_similarity, name='friendsimilarity'),
+    path('movie_category/', views.movie_category, name='movie_category'),
+    path('new_recommend/', views.new_recommend, name='new_recommend'),
+    path('', include("django.contrib.auth.urls")),
+
+
+]
+
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
