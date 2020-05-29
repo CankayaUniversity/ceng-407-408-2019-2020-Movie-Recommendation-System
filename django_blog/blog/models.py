a17d4d854c6bd968ff0b4be297eb2ae99656ed81
dorukgursey <dorukgursey@gmail.com>
2020-05-29 17:21:44 +0300

    update

--- django_blog/blog/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Blog(models.Model):
-    title = models.CharField(max_length = 100 ,verbose_name = 'Baslik Giriniz' , help_text = 'Baslik Bilgisi Burada Girilir' , blank=False , null=True, )
-    icerik = models.TextField(max_length = 1000 , verbose_name = 'Icerik Giriniz',null=True , blank=False)
-    created_date = models.DateField(auto_now_add=True)
