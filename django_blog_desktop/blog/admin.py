c03d91a87094f996fa59e81772af0dae405fd3b1
dorukgursey <dorukgursey@gmail.com>
2020-06-24 17:53:29 +0300

    Latest Code Upload

---
+++ django_blog_desktop/blog/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from.models import  userLiked , comment , friend
+
+admin.site.register(userLiked)
+admin.site.register(comment)
+admin.site.register(friend)
+
+# Register your models here.
+
+
+
